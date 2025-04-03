e8fe65b713a075168d21447f11882a4ac63eae99
Ssekajja Wavamuno Isaac <ssekajjawavamuno@gmail.com>
2025-04-03 22:11:17 +0300

    Add test case for income upto 36_000

--- tax-calc.js
+++ tax-calc.js
@@ -1,6 +1,10 @@
 // Lorna Naula & Ssekajja Wavamuno Isaac
 const calcTax = (income) => {
-  return 0;
+  if (income < 12_000) {
+    return 0;
+  } else if (income >= 12_000 && income <= 36_000) {
+    return 0.2 * income;
+  }
 };
 
 export default calcTax;
--- test/tax-calc-test.js
+++ test/tax-calc-test.js
@@ -6,4 +6,7 @@ describe("Tax calculator test for integers", () => {
   it("should exempt income below 12_000", () => {
     assert.equal(calcTax(10_000), 0);
   });
+  it("should charge 20% for income in range 12_000 to 36_000", () => {
+    assert.equal(calcTax(36_000), 7_200);
+  });
 });
